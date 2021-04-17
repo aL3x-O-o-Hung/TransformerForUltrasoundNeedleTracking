a43a356fec6c6b463edacb754e961b05f50331e9
Wanwen Chen <aliciawwchen@gmail.com>
2021-04-16 21:55:16 -0400

    add dataset

---
+++ needle_image_dataset.py
@@ -0,0 +1,144 @@
+import torch
+from torch.utils.data import Dataset, DataLoader
+from torchvision import transforms
+from PIL import Image
+import os
+
+import numpy as np
+
+
+def read_txt(file_name):
+    folders = []
+    with open(file_name, 'r') as f:
+        for line in f:
+            folder_name = line[:-1]
+            folders.append(folder_name)
+    return folders
+
+### load a pair of image everytime
+class NeedleImagePairDataset(Dataset):
+    
+    def __init__(self, split, root, transform=None, label_transform=None):
+
+        self.split = split
+        self.root = root
+        
+        if self.split in ['train', 'val', 'test']:
+            self.folders = read_txt(os.path.join(self.root, self.split + '.txt'))
+
+        else:
+            raise NotImplementedError("the dataset does not exists!")
+        
+        # load names of image paris
+        self.image_pairs = []
+        self.label_pairs = []
+        for folder in self.folders:
+            file_name = os.listdir(os.path.join(self.root, folder, 'train_images'))
+            file_name.sort(key=lambda x:x[:-4])
+            for f in range(len(file_name) - 1):
+                self.image_pairs.append([os.path.join(folder, 'train_images', file_name[f]), os.path.join(folder, 'train_images', file_name[f + 1])])
+                self.label_pairs.append([os.path.join(folder, 'train_labels', file_name[f]), os.path.join(folder, 'train_labels', file_name[f + 1])])
+
+        if transform == None or label_transform == None:
+            self.transform = [transforms.ToTensor()]
+            self.label_transform = [transforms.ToTensor()]
+        else:
+            self.transform = transform
+            self.label_transform = label_transform
+
+    
+    def __len__(self):
+        return len(self.image_pairs)
+
+
+    def __getitem__(self, idx):
+        image_pair = self.image_pairs[idx]
+        cur_image_name = image_pair[0]
+        next_image_name = image_pair[1]
+
+        label_pair = self.label_pairs[idx]
+        cur_label_name = label_pair[0]
+        next_label_name = label_pair[1]
+        # load image
+        cur_image = Image.open(os.path.join(self.root, cur_image_name))
+        cur_image = transforms.Compose(self.transform)(cur_image)
+        # print(os.path.join(self.root, next_image_name))
+        next_image = Image.open(os.path.join(self.root, next_image_name))
+        next_image = transforms.Compose(self.transform)(next_image) 
+        # load label
+        cur_image_label = Image.open(os.path.join(self.root, cur_label_name))
+        cur_image_label = transforms.Compose(self.transform)(cur_image_label)
+        next_image_label = Image.open(os.path.join(self.root, next_label_name))
+        next_image_label = transforms.Compose(self.transform)(next_image_label)       
+
+        # convert to segmentation map 
+        cur_image_label = cur_image_label[0, :, :]
+        next_image_label = next_image_label[0, :, :]
+
+        return {'current_image':cur_image.float(), 'current_image_label':cur_image_label.long(), 'next_image':next_image.float(), 'next_image_label':next_image_label.long()}
+
+
+### load single image
+class NeedleImageDataset(Dataset):
+    
+    def __init__(self, split, root, transform=None, label_transform=None):
+
+        self.split = split
+        self.root = root
+        
+        if self.split in ['train', 'val', 'test']:
+            self.folders = read_txt(os.path.join(self.root, self.split + '.txt'))
+
+        else:
+            raise NotImplementedError("the dataset does not exists!")
+        
+        # load names of image paris
+        self.image_files = []
+        self.label_files = []
+        for folder in self.folders:
+            file_name = os.listdir(os.path.join(self.root, folder, 'train_images'))
+            file_name.sort(key=lambda x:x[:-4])
+            for f in range(len(file_name)):
+                self.image_files.append(os.path.join(folder, 'train_images', file_name[f]))
+                self.label_files.append(os.path.join(folder, 'train_labels', file_name[f]))
+
+        if transform == None or label_transform == None:
+            self.transform = [transforms.ToTensor()]
+            self.label_transform = [transforms.ToTensor()]
+        else:
+            self.transform = transform
+            self.label_transform = label_transform
+
+    
+    def __len__(self):
+        return len(self.image_files)
+
+
+    def __getitem__(self, idx):
+        image_name = self.image_files[idx]
+
+        label_name = self.label_files[idx]
+
+        # load image
+        cur_image = Image.open(os.path.join(self.root, image_name))
+        cur_image = transforms.Compose(self.transform)(cur_image)
+
+        # load label
+        cur_image_label = Image.open(os.path.join(self.root, label_name))
+        cur_image_label = transforms.Compose(self.transform)(cur_image_label)
+
+        # convert to segmentation map 
+        cur_image_label = cur_image_label[0, :, :]
+
+        return {'current_image':cur_image.float(), 'current_image_label':cur_image_label.long()}
+
+
+if __name__ == '__main__':
+    # l = read_txt("../needle_insertion_dataset/train.txt")
+    # print(l)
+
+    dataset = NeedleImagePairDataset(split='train', root='../needle_insertion_dataset')
+    data = dataset.__getitem__(0)
+
+    print(data['current_image'].type())
+    print(data['current_image_label'].size())
